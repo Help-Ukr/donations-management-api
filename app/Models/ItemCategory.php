29ab8879d6bfa34833ba88823ffc9379ce17b2b3
Eugene A <jax21ukr@gmail.com>
2022-04-09 12:23:49 +0300

    ItemCategory field renamed

--- app/Models/ItemCategory.php
+++ app/Models/ItemCategory.php
@@ -13,9 +13,9 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @OA\Schema(
  *     schema="ItemCategory",
- *     required={"id", "item_category_id", "name", "icon"},
+ *     required={"id", "parent_id", "name", "icon"},
  *     @OA\Property(property="id", type="number", title="Id", example="1"),
- *     @OA\Property(property="item_category_id", type="number", title="Parent Id", example="1"),
+ *     @OA\Property(property="parent_id", type="number", title="Parent Id", example="1"),
  *     @OA\Property(property="name", type="string", title="Username", example="food"),
  *     @OA\Property(property="icon", type="string", title="icon", example="🔦"),
  * )
