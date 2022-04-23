32573ca52250cd6d140bd9f6e7b041b957fd4ce7
Eugene A <jax21ukr@gmail.com>
2022-04-23 14:52:39 +0300

    ItemCategoryTableSeeder updated

--- database/seeders/ItemCategoryTableSeeder.php
+++ database/seeders/ItemCategoryTableSeeder.php
@@ -16,19 +16,19 @@ class ItemCategoryTableSeeder extends Seeder
         \DB::table('item_categories')->truncate();
 
         \DB::table('item_categories')->insert([
-            [ 'id' => 1, 'parent_id' => NULL, 'icon' => "🍜🥫🥔🥕", 'name' => "Lebensmittel" ],
-            [ 'id' => 2, 'parent_id' => NULL, 'icon' => "🪥🧻", 'name' => "Hygieneartikel" ],
-            [ 'id' => 3, 'parent_id' => NULL, 'icon' => "👚👖", 'name' => "Textilien und Kleider" ],
-            [ 'id' => 4, 'parent_id' => NULL, 'icon' => "🛏️🪑", 'name' => "Möbelstücke" ],
-            [ 'id' => 5, 'parent_id' => NULL, 'icon' => "🔌🔦📟", 'name' => "Elektronik" ],
-            [ 'id' => 6, 'parent_id' => NULL, 'icon' => "🥄🍴", 'name' => "Küchengeräte" ],
-            [ 'id' => 7, 'parent_id' => NULL, 'icon' => "🍽️", 'name' => "Geschirr" ],
-            [ 'id' => 8, 'parent_id' => NULL, 'icon' => "🏐🥊⛸️🏓", 'name' => "Sportartausrüstung" ],
+            [ 'id' => 1, 'parent_id' => NULL, 'icon' => "🔌🔦📟", 'name' => "Elektronik" ],
+            [ 'id' => 2, 'parent_id' => NULL, 'icon' => "🍽️", 'name' => "Geschirr" ],
+            [ 'id' => 3, 'parent_id' => NULL, 'icon' => "🗿🖨️", 'name' => "Großgeräte" ],
+            [ 'id' => 4, 'parent_id' => NULL, 'icon' => "🪥🧻", 'name' => "Hygieneartikel" ],
+            [ 'id' => 5, 'parent_id' => NULL, 'icon' => "🥄🍴", 'name' => "Küchengeräte" ],
+            [ 'id' => 6, 'parent_id' => NULL, 'icon' => "🍜🥫🥔🥕", 'name' => "Lebensmittel" ],
+            [ 'id' => 7, 'parent_id' => NULL, 'icon' => "🛌", 'name' => "Matratzen" ],
+            [ 'id' => 8, 'parent_id' => NULL, 'icon' => "💉💊", 'name' => "Medikamente" ],
             [ 'id' => 9, 'parent_id' => NULL, 'icon' => "🩺🚑🌡️", 'name' => "Medizinische Ausrüstung" ],
-            [ 'id' => 10, 'parent_id' => NULL, 'icon' => "🛌", 'name' => "Matratzen" ],
-            [ 'id' => 11, 'parent_id' => NULL, 'icon' => "💉💊", 'name' => "Medikamente" ],
-            [ 'id' => 12, 'parent_id' => NULL, 'icon' => "🪖🦺🛡️", 'name' => "Militärausrüstung" ],
-            [ 'id' => 13, 'parent_id' => NULL, 'icon' => "🗿🖨️", 'name' => "Großgeräte" ],
+            [ 'id' => 10, 'parent_id' => NULL, 'icon' => "🪖🦺🛡️", 'name' => "Militärausrüstung" ],
+            [ 'id' => 11, 'parent_id' => NULL, 'icon' => "🛏️🪑", 'name' => "Möbelstücke" ],
+            [ 'id' => 12, 'parent_id' => NULL, 'icon' => "🏐🥊⛸️🏓", 'name' => "Sportartausrüstung" ],
+            [ 'id' => 13, 'parent_id' => NULL, 'icon' => "👚👖", 'name' => "Textilien und Kleider" ],
         ]);
     }
 }
